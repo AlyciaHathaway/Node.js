525c305f8f4839e2542310dda46c1e0639d272c9
AlyciaHathaway <Alyciahathaways@gmail.com>
2017-05-23 17:58:37 +0800

    test npm

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 
 var another = require('./test.js');
-
 console.log(another);
+// 查找文件优先级 .js .json .node
+var package = require('./package');
+console.log(package);
+// node查找路径的方式
+console.log(module.paths);
--- test.js
+++ test.js
@@ -1,7 +1,10 @@
+//第一种导出模块方式
+//导出一个hello，称之为'接口'
+//exports是module.exports的简写
+exports.hello = 'world';
 
-// exports.hello = 'world';
-
+// 第二种导出模块方式
 var hello = 'world';
 module.exports = {
 	hello: hello
-}
+};
